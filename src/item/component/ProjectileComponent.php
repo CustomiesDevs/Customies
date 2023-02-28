ee390915fef1184f680d2308e48ec24a4c24e1d4
morcheysha77 <laurentmorgan77@gmail.com>
2023-02-28 21:51:55 +0100

    Update ProjectileComponent.php

--- src/item/component/ProjectileComponent.php
+++ src/item/component/ProjectileComponent.php
@@ -3,25 +3,11 @@ declare(strict_types=1);
 
 namespace customiesdevs\customies\item\component;
 
-final class ProjectileComponent implements ItemComponent {
-
-	private string $projectileEntity;
+final class ProjectileComponent extends BasicComponent {
 
 	public function __construct(string $projectileEntity) {
-		$this->projectileEntity = $projectileEntity;
-	}
-
-	public function getName(): string {
-		return "minecraft:projectile";
-	}
-
-	public function getValue(): array {
-		return [
-			"projectile_entity" => $this->projectileEntity
-		];
-	}
-
-	public function isProperty(): bool {
-		return false;
+        parent::__construct("minecraft:projectile", [
+            "entity" => $projectileEntity
+        ], false);
 	}
 }
