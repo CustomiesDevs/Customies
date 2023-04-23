d9c02eb8ef6b0d467089d522ce2fe552a6d5b17d
xLordShadow <tylermk166@gmail.com>
2023-04-22 23:05:50 -0400

    Update ShooterComponent.php
    Provided brief descriptions of each of the components parameters and removed the constructor.

--- src/item/component/ShooterComponent.php
+++ src/item/component/ShooterComponent.php
@@ -5,20 +5,13 @@ namespace customiesdevs\customies\item\component;
 
 final class ShooterComponent implements ItemComponent {
 
-  //TODO Ammunition
-  private bool $chargeOnDraw;
-  private float $launchPowerScale;
-  private float $maxDrawDuration;
-  private float $maxLaunchPower;
-  private bool $scalePowerByDuration;
 
-  public function __construct(bool $chargeOnDraw = false, float $launchPowerScale = 1.0, float $maxDrawDuration = 0, float $maxLaunchPower = 1.0, bool $scalePowerByDuration = false) {
-    $this->chargeOnDraw = $chargeOnDraw;
-    $this->launchPowerScale = $launchPowerScale;
-    $this->maxDrawDuration = $maxDrawDuration;
-    $this->maxLaunchPower = $maxLaunchPower;
-    $this->scalePowerByDuration = $scalePowerByDuration;
-  }
+  private bool $chargeOnDraw = false;//Sets if the item is charged when drawn
+  private float $launchPowerScale = 1.0;//Scale used for the launch power
+  private float $maxDrawDuration = 0;//How long can it be drawn before it will release automatically
+  private float $maxLaunchPower = 1.0;//Launch power
+  private bool $scalePowerByDuration = false;//Scale the power by draw duration? When true, the longer you hold, the more power it will have when released.
+  private string $ammunition = "";//Sets the entity that is used as ammunition
 
   public function getName(): string {
     return "minecraft:shooter";
@@ -30,7 +23,8 @@ final class ShooterComponent implements ItemComponent {
       "launch_power_scale" => $this->launchPowerScale,
       "max_draw_duration" => $this->maxDrawDuration,
       "max_launch_power" => $this->maxLaunchPower,
-      "scale_power_by_draw_duration" => $this->scalePowerByDuration
+      "scale_power_by_draw_duration" => $this->scalePowerByDuration,
+      "ammunition" => $this->ammunition
     ];
   }
 
