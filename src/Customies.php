de07906958acc408cd370e028820f6637a72fedb
royaljacquesytb <maximejacques667@gmail.com>
2023-06-21 21:20:47 +0200

    add giveItem command

--- src/Customies.php
+++ src/Customies.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 namespace customiesdevs\customies;
 
 use customiesdevs\customies\block\CustomiesBlockFactory;
+use customiesdevs\customies\commands\GiveItem;
 use customiesdevs\customies\util\Cache;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
+use pocketmine\Server;
 
 final class Customies extends PluginBase {
 
@@ -24,5 +26,6 @@ final class Customies extends PluginBase {
 			Cache::getInstance()->save();
 			CustomiesBlockFactory::getInstance()->addWorkerInitHook($cachePath);
 		}), 0);
+        Server::getInstance()->getCommandMap()->register("customies", new GiveItem());
 	}
 }
---
+++ src/commands/GiveItem.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace customiesdevs\customies\commands;
+
+use customiesdevs\customies\item\CustomiesItemFactory;
+use pocketmine\command\CommandSender;
+use pocketmine\command\defaults\VanillaCommand;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\DefaultPermissions;
+
+class GiveItem extends VanillaCommand{
+    public function __construct()
+    {
+        parent::__construct("cgiveitem", "give item from customies plugin", "/give <pseudo> <item> <count>", ["cgi"]);
+
+        $this->setPermission(DefaultPermissions::ROOT_USER);
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args): void
+    {
+        if(count($args) < 2){
+            $sender->sendMessage("§cUsage: /cgi <pseudo> <item> <count>");
+            return;
+        }
+        $player = $sender->getServer()->getPlayerByPrefix($args[0]);
+        if($player === null){
+            $sender->sendMessage("§cPlayer not found");
+            return;
+        }
+        try {
+            CustomiesItemFactory::getInstance()->get($args[1]);
+        }catch (\Exception $exception){
+            $sender->sendMessage("§cItem not found");
+            return;
+        }
+        if (isset($args[2])) {
+            if (is_numeric($args[2])) {
+                $player->getInventory()->addItem(CustomiesItemFactory::getInstance()->get($args[1], $args[2]));
+                $sender->sendMessage("§aItem given");
+            } else {
+                $sender->sendMessage("§cCount must be a number");
+            }
+        } else {
+            $player->getInventory()->addItem(CustomiesItemFactory::getInstance()->get($args[1]));
+        }
+    }
+}
