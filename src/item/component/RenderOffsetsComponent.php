b993ae64cae9ab96a8ddf084e74aa10268f18728
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2024-01-24 08:39:56 +0100

    Fix RenderOffsets
    This may (and will) lead to API BC

--- src/item/component/RenderOffsetsComponent.php
+++ src/item/component/RenderOffsetsComponent.php
@@ -3,16 +3,13 @@ declare(strict_types=1);
 
 namespace customiesdevs\customies\item\component;
 
-final class RenderOffsetsComponent implements ItemComponent {
 
-	private int $textureWidth;
-	private int $textureHeight;
-	private bool $handEquipped;
+final class RenderOffsetsComponent implements ItemComponent { //thanks to https://github.com/MedicalJewel105/bedrock-render-offsets-generator/blob/main/main.py
 
-	public function __construct(int $textureWidth, int $textureHeight, bool $handEquipped = false) {
-		$this->textureWidth = $textureWidth;
-		$this->textureHeight = $textureHeight;
-		$this->handEquipped = $handEquipped;
+	private int $textureSize;
+
+	public function __construct(int $textureSize) {
+		$this->textureSize = $textureSize;
 	}
 
 	public function getName(): string {
@@ -20,19 +17,28 @@ final class RenderOffsetsComponent implements ItemComponent {
 	}
 
 	public function getValue(): array {
-		$horizontal = ($this->handEquipped ? 0.075 : 0.1) / ($this->textureWidth / 16);
-		$vertical = ($this->handEquipped ? 0.125 : 0.1) / ($this->textureHeight / 16);
-		$perspectives = [
-			"first_person" => [
-				"scale" => [$horizontal, $vertical, $horizontal],
-			],
-			"third_person" => [
-				"scale" => [$horizontal, $vertical, $horizontal]
-			]
-		];
+	        $textureSize = $this->textureSize;
+	        $mainHand_fp = round(0.039 * 16 / $textureSize, 8);
+	        $offhand_fp = round(0.065 * 16 / $textureSize, 8);
+	        $mainHand_tp = $offhand_tp = round(0.0965 * 16 / $textureSize, 8);
+
 		return [
-			"main_hand" => $perspectives,
-			"off_hand" => $perspectives
+			"main_hand" => [
+				"first_person" => [
+					"scale" => [$mainHand_fp, $mainHand_fp, $mainHand_fp],
+				],
+				"third_person" => [
+					"scale" => [$mainHand_tp, $mainHand_tp, $mainHand_tp]
+				]
+			],
+			"off_hand" => [
+				"first_person" => [
+					"scale" => [$offhand_fp, $offhand_fp, $offhand_fp],
+				],
+				"third_person" => [
+					"scale" => [$offhand_tp, $offhand_tp, $offhand_tp]
+				]
+			]
 		];
 	}
 
