eff55d16c2560839fff05348f1e6ea5e269b755f
GoGoAirPlane <neoskr@outlook.kr>
2024-03-27 03:16:56 +0900

    ✨ 1.5.0 : minecraft:collision_box와 minecraft:selection_box를 각각 설정 할 수 있도록 수정

--- src/block/Model.php
+++ src/block/Model.php
@@ -13,17 +13,28 @@ final class Model {
 	/** @var Material[] */
 	private array $materials;
 	private ?string $geometry;
-	private Vector3 $origin;
-	private Vector3 $size;
+    private Vector3 $originForCollisionBox;
+    private Vector3 $sizeForCollisionBox;
+	private Vector3 $originForSelectionBox;
+	private Vector3 $sizeForSelectionBox;
 
 	/**
 	 * @param Material[] $materials
 	 */
-	public function __construct(array $materials, ?string $geometry = null, ?Vector3 $origin = null, ?Vector3 $size = null) {
+	public function __construct(
+        array $materials,
+        ?string $geometry = null,
+        ?Vector3 $originForCollisionBox = null,
+        ?Vector3 $sizeForCollisionBox = null,
+        ?Vector3 $originForSelectionBox = null,
+        ?Vector3 $sizeForSelectionBox = null
+    ){
 		$this->materials = $materials;
 		$this->geometry = $geometry;
-		$this->origin = $origin ?? Vector3::zero();
-		$this->size = $size ?? Vector3::zero();
+		$this->originForCollisionBox = $originForCollisionBox ?? Vector3::zero();
+		$this->sizeForCollisionBox = $sizeForCollisionBox ?? Vector3::zero();
+        $this->originForSelectionBox = $originForSelectionBox ?? Vector3::zero();
+        $this->sizeForSelectionBox = $sizeForSelectionBox ?? Vector3::zero();
 	}
 
 	/**
@@ -49,26 +60,26 @@ final class Model {
 			$material["minecraft:collision_box"] = CompoundTag::create()
 				->setByte("enabled", 1)
 				->setTag("origin", new ListTag([
-					new FloatTag($this->origin->getX()),
-					new FloatTag($this->origin->getY()),
-					new FloatTag($this->origin->getZ())
+					new FloatTag($this->originForCollisionBox->getX()),
+					new FloatTag($this->originForCollisionBox->getY()),
+					new FloatTag($this->originForCollisionBox->getZ())
 				]))
 				->setTag("size", new ListTag([
-					new FloatTag($this->size->getX()),
-					new FloatTag($this->size->getY()),
-					new FloatTag($this->size->getZ())
+					new FloatTag($this->sizeForCollisionBox->getX()),
+					new FloatTag($this->sizeForCollisionBox->getY()),
+					new FloatTag($this->sizeForCollisionBox->getZ())
 				]));
 			$material["minecraft:selection_box"] = CompoundTag::create()
 				->setByte("enabled", 1)
 				->setTag("origin", new ListTag([
-					new FloatTag($this->origin->getX()),
-					new FloatTag($this->origin->getY()),
-					new FloatTag($this->origin->getZ())
+					new FloatTag($this->originForSelectionBox->getX()),
+					new FloatTag($this->originForSelectionBox->getY()),
+					new FloatTag($this->originForSelectionBox->getZ())
 				]))
 				->setTag("size", new ListTag([
-					new FloatTag($this->size->getX()),
-					new FloatTag($this->size->getY()),
-					new FloatTag($this->size->getZ())
+					new FloatTag($this->sizeForSelectionBox->getX()),
+					new FloatTag($this->sizeForSelectionBox->getY()),
+					new FloatTag($this->sizeForSelectionBox->getZ())
 				]));
 		}
 		return $material;
