6931068779377ce94b97d5a637cc2fc4d4287342
Vasync <tbao12901@gmail.com>
2024-08-24 02:57:10 +0700

    Update Pickaxe.php

--- src/item/types/Pickaxe.php
+++ src/item/types/Pickaxe.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace customiesdevs\customies\item\types;
 
+use customiesdevs\customies\item\component\MiningSpeedComponent;
+use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\Pickaxe as PM_Pickaxe;
 
 class Pickaxe extends PM_Pickaxe {
 	
 	/* Example:
-	public function __construct(ItemIdentifier $identifier, string $name = "Unknown") {
-		parent::__construct($identifier, $name, \pocketmine\item\ToolTier::NETHERITE());
-		$this->addComponent(new MiningSpeedComponent($this->getTypePickaxe(), $this->getMiningEfficiency(true)));
-	}
+	*  public function __construct(ItemIdentifier $identifier, string $name = "Unknown") {
+	*	 parent::__construct($identifier, $name, \pocketmine\item\ToolTier::NETHERITE());
+	*	 $this->addComponent(new MiningSpeedComponent($this->getTypePickaxe(), $this->getMiningEfficiency(true)));
+	*  }
 	*/
 
 	public function getTypePickaxe(): string {
@@ -27,8 +29,19 @@ class Pickaxe extends PM_Pickaxe {
 
 	#[Override]
 	public function addEnchantment(EnchantmentInstance $enchantment) : self{
-		$this->updateEfficiency();
 		$this->enchantments[spl_object_id($enchantment->getType())] = $enchantment;
+		$this->updateEfficiency();
+		return $this;
+	}
+
+	#[Override]
+	public function removeEnchantment(Enchantment $enchantment, int $level = -1) : self{
+		$instance = $this->getEnchantment($enchantment);
+		if($instance !== null && ($level === -1 || $instance->getLevel() === $level)){
+			unset($this->enchantments[spl_object_id($enchantment)]);
+		}
+		$this->updateEfficiency();
+
 		return $this;
 	}
 	
