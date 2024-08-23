5f0a68fa8caff45e7b80da24bd2b603fa7e27f59
Vasync <tbao12901@gmail.com>
2024-08-24 01:37:49 +0700

    Update MiningSpeedComponent.php

--- src/item/component/MiningSpeedComponent.php
+++ src/item/component/MiningSpeedComponent.php
@@ -3,23 +3,96 @@ declare(strict_types=1);
 
 namespace customiesdevs\customies\item\component;
 
+use pocketmine\block\Block;
+use pocketmine\world\format\io\GlobalBlockStateHandlers;
+use function array_map;
+use function implode;
+
 final class MiningSpeedComponent implements ItemComponent {
 
-	private int $miningSpeed;
+	private array $destroySpeeds;
+	private array TOOL_TYPES = ["wood", "stone", "iron", "gold", "diamond"];
 
-        public function __construct(int $mspeed) {
-            $this->miningSpeed = $mspeed;
+        public function __construct(private string $toolType = "wood", private int $mspeed) {
+		if (!in_array($this->toolType, self::TOOL_TYPES)) {
+			try {
+				throw new Exception('Tool type not listed, default is wood!');
+			} catch (Exception $e) {
+                                echo 'Tool Mining Speed Error: ',  $e->getMessage();
+                        }
+		}
+
+		$this->withBlocks($this->mspeed,
+			"Andesite", 
+			"Andesite Wall", 
+			"Brick Slab", 
+			"Brick Wall", 
+			"Bricks", 
+			"Cobblestone", 
+			"Cobblestone Slab", 
+			"Cobblestone Stairs", 
+			"Cobblestone Wall", 
+			"Diorite", 
+			"Diorite Wall", 
+			"Dripstone Block", 
+			"End Stone Brick Wall", 
+			"Granite", 
+			"Granite Wall", 
+			"Mossy Cobblestone", 
+			"Mossy Cobblestone Wall", 
+			"Nether Brick Wall", 
+			"Polished Andesite", 
+			"Polished Diorite", 
+			"Polished Granite", 
+			"Prismarine Wall", 
+			"Quartz Slab", 
+			"Red Nether Brick Wall", 
+			"Red Sandstone Wall", 
+			"Sandstone Slab", 
+			"Sandstone Wall", 
+			"Smooth Stone Slab", 
+			"Stone", 
+			"Stone Brick Wall", 
+			"Stone Bricks Slab"
+			);
+		
+                $this->withTags($this->mspeed, 'metal', $this->toolType.'_pick_diggable'));
         }
   
 	public function getName(): string {
-		return "minecraft:mining_speed";
+		return "minecraft:digger";
 	}
 
-	public function getValue(): int {
-		return $this->miningSpeed;
+	public function getValue(): array {
+		return [
+			"destroy_speeds" => $this->destroySpeeds
+		];
 	}
 
 	public function isProperty(): bool {
 		return false;
-        }
+	}
+
+	public function withBlocks(int $speed, string ...$blocks): DiggerComponent {
+		foreach($blocks as $block){
+			$this->destroySpeeds[] = [
+				"block" => [
+					"name" => "minecraft:". str_replace(" ", "_", strtolower($block))
+				],
+				"speed" => $speed
+			];
+		}
+		return $this;
+	}
+
+	public function withTags(int $speed, string ...$tags): DiggerComponent {
+		$query = implode(",", array_map(fn($tag) => "'" . $tag . "'", $tags));
+		$this->destroySpeeds[] = [
+			"block" => [
+				"tags" => "query.any_tag(" . $query . ")"
+			],
+			"speed" => $speed
+		];
+		return $this;
+	}
 }
