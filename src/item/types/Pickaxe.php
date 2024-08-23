b1b087433d78f3814be6c2441fd3f83de60c6104
Vasync <tbao12901@gmail.com>
2024-08-24 02:46:25 +0700

    Update Pickaxe.php

--- src/item/types/Pickaxe.php
+++ src/item/types/Pickaxe.php
@@ -7,7 +7,17 @@ namespace customiesdevs\customies\item\types;
 use pocketmine\item\Pickaxe as PM_Pickaxe;
 
 class Pickaxe extends PM_Pickaxe {
-        
+
+	public function __construct(ItemIdentifier $identifier, string $name = "Unknown") {
+		parent::__construct($identifier, $name, \pocketmine\item\ToolTier::NETHERITE());
+		$this->addComponent(new MiningSpeedComponent("diamond", $this->getMiningEfficiency(true)));
+	}
+	
+	public function updateEfficiency(int $upLV): void{
+		$this->removeMiningSpeedComponent();
+		$this->addComponent(new MiningSpeedComponent("diamond", $this->getMiningEfficiency(true)));
+	}
+	
         #[Override]
         public function getMiningEfficiency(bool $isCorrectTool) : float{
 		$efficiency = 1;
@@ -19,7 +29,7 @@ class Pickaxe extends PM_Pickaxe {
 		}
 
 		return $efficiency;
-        }
+	}
         
         #[Override]
 	protected function getBaseMiningEfficiency() : float{
